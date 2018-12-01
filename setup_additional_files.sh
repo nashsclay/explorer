05babe5a724eeb119567bac792dc0ba93a17e75b
nashsclay <nashsclay@gmail.com>
2018-12-01 12:06:30 -0700

    Update setup_additional_files.sh

--- setup_additional_files.sh
+++ setup_additional_files.sh
@@ -5,5 +5,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 0.10.28
-nohup npm start 2> /dev/null &
 exit 1
