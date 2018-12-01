2ccbcbbec143de9064630ccb3e279d357890ab1d
nashsclay <nashsclay@gmail.com>
2018-12-01 11:36:44 -0700

    Update setup_mongo.sh

--- setup_mongo.sh
+++ setup_mongo.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update
