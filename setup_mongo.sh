728a005a7645e3e1fb660c9e992c9568bac99e61
nashsclay <nashsclay@gmail.com>
2018-12-01 11:12:00 -0700

    Create setup_mongo.sh

---
+++ setup_mongo.sh
@@ -0,0 +1,8 @@
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update
+sudo apt-get install mongodb-org=2.6.2 mongodb-org-server=2.6.2 mongodb-org-shell=2.6.2 mongodb-org-mongos=2.6.2 mongodb-org-tools=2.6.2
+sudo ufw allow 27017
+sudo mkdir -p /data/db/
+sudo chown `id -u` /data/db
+nohup mongod &
